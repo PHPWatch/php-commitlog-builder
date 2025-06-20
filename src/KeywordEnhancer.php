7ff9171f0f5e668ae06e46ff8f4eb9ac18f70adc
Ayesh Karunaratne <ayesh@aye.sh>
2025-06-20 18:07:12 +0530

    Keywords: improve class method detection to allow numbers in the class name

--- src/KeywordEnhancer.php
+++ src/KeywordEnhancer.php
@@ -8,7 +8,7 @@ class KeywordEnhancer {
         '/\b(?<!`)(?:zend|php|_php)_[a-z_*]+\b(?![`.(=])\*?/i', // zend_foo_bar
         '/\b(?<![`\/])[a-z][a-z\d_-]+(.stubs?)?\.(phpt?|c|h)(?![`.?])/', // run-tests.php / foo.stub.php foo.stubs.php / test-foo-bar.phpt,
         '/\b(?<!`)ext\/[a-z_]+\b(?![`\/])/', // ext-names
-        '/\b(?<!`)[A-Za-z][A-Za-z]+::(?:__)?[a-z][A-Za-z\d_]+\(\)(?![`\/-])/', // Class::methods()
+        '/\b(?<!`)[A-Za-z][A-Za-z\d]+::(?:__)?[a-z][A-Za-z\d_]+\(\)(?![`\/-])/', // Class::methods()
         '/\b(?<!`)[A-Z][A-Za-z]+::(?:__)[a-z][A-Za-z\d_]+(?![`\/-])\b(?![)(])/', // Class::__magicMethods
         '/\b(?<!`)[A-Z][A-Za-z]+::[A-Z_]+\b(?![`\/(])/', // Class::CONSTANTS
         '/\b(?<![`\\\\])[A-Z][A-Z\\\\a-z]+::[a-z][A-Za-z_\d]+\b(?![`\/(])/', // Class::constants
